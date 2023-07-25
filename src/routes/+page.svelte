988f662080b4cc373d35dc59da81c3f02e8e4492
Esen Dzhumaev <dzhumaev.esen@auca.kg>
2023-07-25 12:28:46 +0600

    add deta skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
 <script>
+    // import { Deta } from "deta";
 
+    // const deta = Deta();
+    // const db = Base();
+
+    // let o = {
+    //     key: 1,
+    //     name: ""
+    // }
+
+    // async function asdf() {
+    //     let result = await db.put(o);
+    // }
 </script>
 
 <h1>Hello World!</h1>
