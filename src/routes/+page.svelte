d06a5be77be03ec8d376d9d80cd25ceeba7652b6
Esen Dzhumaev <dzhumaev.esen@auca.kg>
2023-07-28 04:05:29 +0600

    add store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,50 @@
 <script>
-    // import { Deta } from "deta";
+    export let animeList;
+    import { Deta } from "deta";
 
-    // const deta = Deta();
-    // const db = Base();
+    const deta = Deta("project_key");
+    const db = deta.Base("Data");
 
-    // let o = {
-    //     key: 1,
-    //     name: ""
-    // }
+    let o = {
+        key: 1,
+        name: ""
+    }
 
-    // async function asdf() {
-    //     let result = await db.put(o);
-    // }
+    async function asdf() {
+        let result = await db.put(o);
+    }
 </script>
 
-<h1>Hello World!</h1>
+<div class="anime-list">
+    {#each animeList as anime}
+        <div class="anime-card">
+            <img src={anime.image} alt={anime.title} />
+            <h2>{anime.title}</h2>
+            <p>Rating: {anime.rating}</p>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .anime-list {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        gap: 20px;
+        justify-content: center;
+        align-items: center;
+    }
+  
+    .anime-card {
+        border: 1px solid #ccc;
+        padding: 10px;
+        border-radius: 8px;
+        text-align: center;
+    }
+  
+    .anime-card img {
+        width: 200px;
+        height: 250px;
+        object-fit: cover;
+        border-radius: 8px;
+    }
+</style>
